a0520d7b56b263eea60935e26568ad9f69ba2225
renzon <renzon@gmail.com>
2017-03-14 22:59:01 -0300

    close #2

--- calculadora/oo/main.py
+++ calculadora/oo/main.py
@@ -1,5 +1,16 @@
-from calculadora.oo.framework import Calculadora, CalculadoraInfixa
+from calculadora.oo.framework import CalculadoraInfixa, Calculadora
 
-calculadora = CalculadoraInfixa()
+
+class CalculadoraPrefixa(Calculadora):
+    def obter_inputs(self):
+        sinal = input('Digite o sinal da operação (*, + ou -): ')
+        p1 = input('Digite o primeiro número: ')
+        p1 = float(p1)
+        p2 = input('Digite o segundo número: ')
+        p2 = float(p2)
+        return sinal, p1, p2
+
+
+calculadora = CalculadoraPrefixa()
 
 print(calculadora.efetuar_operacao())
